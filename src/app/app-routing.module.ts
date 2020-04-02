10ef6de869778be3723a5b7bde69517718c6bcfc
paul <paul.18.sma@gmail.com>
2020-04-02 19:45:51 -0300

    moves prescription form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ValidatorFormComponent } from './components/prescriptions/validator-form/validator-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:  ValidatorFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  ValidatorFormComponent
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DrugsService } from './services/drugs.service';
-import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { startWith, map, tap } from 'rxjs/operators';
-import Drugs from './interfaces/drugs';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,75 +6,8 @@ import Drugs from './interfaces/drugs';
 })
 export class AppComponent implements OnInit {
 
-  title = 'preinscriptions-control';
-  prescriptionForm: FormGroup;
-  today;
+  constructor(){}
 
-  options: string[] = [];
-  drugs: Drugs[] = [];
-  filteredOptions: Observable<string[]>;
+  ngOnInit(){}
 
-  constructor(private drugsService: DrugsService, private fBuilder: FormBuilder){}
-
-  ngOnInit(){
-    this.initPrescriptionForm();
-    this.today = new Date();
-
-    this.prescriptionForm.get('supply').valueChanges.subscribe(
-      term => {
-        this.getDrugs(term);
-      }
-    )
-
-  }
-
-  initPrescriptionForm(){
-    this.prescriptionForm = this.fBuilder.group({
-      dni: ['', [
-        Validators.required,
-        Validators.minLength(8)
-      ]],
-      date: ['', [
-        Validators.required
-      ]],
-      professional: ['', [
-        Validators.required
-      ]],
-      supply: ['', [
-        Validators.required
-      ]],
-    });
-  }
-
-  getDrugs(term: string):void{
-    if(term.length > 2){
-
-      this.drugsService.getDrugByTerm(term).subscribe(
-        res => {
-          this.drugs = res.items;
-        },
-      );
-    }
-  }
-
-  onSubmitPrescriptionForm(){
-    console.log('page under construction');
-  }
-
-
-  get dni(): AbstractControl{
-    return this.prescriptionForm.get('dni');
-  }
-
-  get date(): AbstractControl{
-    return this.prescriptionForm.get('date');
-  }
-
-  get professional(): AbstractControl{
-    return this.prescriptionForm.get('professional');
-  }
-
-  get supply(): AbstractControl{
-    return this.prescriptionForm.get('supply');
-  }
 }
