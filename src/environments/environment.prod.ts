7b95fd2040609f6aa975514727efad302a9f0282
paul <paul.18.sma@gmail.com>
2020-04-02 20:11:23 -0300

    changes prod api_end_point

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_END_POINT: 'htthttps://prescription-validator-api.herokuapp.com',
+  API_END_POINT: 'https://prescription-validator-api.herokuapp.com/api',
   SNOMED_API: 'https://browser.ihtsdotools.org/snowstorm/snomed-ct/MAIN/SNOMEDCT-ES/SNOMEDCT-AR/2019-11-30/descriptions?limit=10&',
   SNOMED_PARAMS: '&lang=spanish&conceptActive=false&semanticTags=product',
   ANDES_API: 'https://app.andes.gob.ar/api'
