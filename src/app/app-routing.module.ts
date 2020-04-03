4b20c8ae45cccc4a18b617764a2409708d9661f2
paul <paul.18.sma@gmail.com>
2020-04-03 01:30:31 -0300

    adds catch 404 page to redirect a 404 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ValidatorFormComponent } from './components/prescriptions/validator-form/validator-form.component';
+import { AuthGuard } from '@auth/guards/auth.guard';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component:  ValidatorFormComponent
-  }
+    canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'prescriptions/form',
+      },
+      {
+        path: 'prescriptions/form',
+        component: ValidatorFormComponent
+      }
+    ]
+  },
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
@@ -17,5 +33,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 export const routingComponents = [
-  ValidatorFormComponent
+  ValidatorFormComponent,
+  NotFoundComponent
 ]
