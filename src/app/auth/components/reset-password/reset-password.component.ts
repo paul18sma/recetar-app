5d48983554b8f422316870cfb1300bf06aa674ec
paul <paul.18.sma@gmail.com>
2020-04-15 00:43:45 -0300

    adds reset password form

--- src/app/auth/components/reset-password/reset-password.component.ts
+++ src/app/auth/components/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, FormGroupDirective, Validators } from '@angular/forms';
+import { AuthService } from '@auth/services/auth.service';
+import { Router } from '@angular/router';
+import { ThemePalette } from '@angular/material/core/common-behaviors/color';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  resetForm: FormGroup;
+  hideOldPassword: boolean = true;
+  hideNewPassword: boolean = true;
+  error: string;
+  isLoading: boolean = false;
+  readonly spinnerColor: ThemePalette = 'accent';
+
+  constructor(private fBuild: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.initResetForm();
   }
 
+  initResetForm(){
+    this.resetForm = this.fBuild.group({
+      oldPassword: ['', [
+        Validators.required
+      ]],
+      newPassword: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    });
+  }
+
+  onSubmitEvent(resetForm: FormGroup, resetNgForm: FormGroupDirective): void{
+    if(this.resetForm.valid){
+
+      this.authService.resetPassword(this.resetForm.value).subscribe(
+        res => {
+          // menssage
+          setTimeout(() => {
+            if(this.authService.isPharmacistsRole()){
+              this.router.navigate(['/farmacias/recetas/dispensar']);
+            } else if(this.authService.isProfessionalRole()){
+              this.router.navigate(['/profesionales/recetas/nueva']);
+            }
+          }, 5000);
+        },
+        err => {
+          resetNgForm.resetForm();
+          resetForm.reset();
+          this.error = err;
+      });
+    }
+  }
+
+  get oldPassword(): AbstractControl {
+    return this.resetForm.get('oldPassword');
+  }
+
+  get newPassword(): AbstractControl {
+    return this.resetForm.get('newPassword');
+  }
 }
