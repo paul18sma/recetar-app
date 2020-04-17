e48a77bdf6305e2c0d27b478633dcc77ae79df80
BasHud <eugesma@gmail.com>
2020-04-16 21:33:46 -0300

    Change user_id tu user

--- src/app/professionals/components/professional-form/professional-form.component.ts
+++ src/app/professionals/components/professional-form/professional-form.component.ts
@@ -66,7 +66,7 @@ export class ProfessionalFormComponent implements OnInit {
   initProfessionalForm(){
     this.today = new Date((new Date()));
     this.professionalForm = this.fBuilder.group({
-      user_id: [this.authService.getLoggedUserId()],
+      user: [this.authService.getLoggedUserId()],
       professionalFullname: [''],
       patient: this.fBuilder.group({
         dni: ['', [
@@ -145,12 +145,12 @@ export class ProfessionalFormComponent implements OnInit {
       this.apiPrescriptions.newPrescription(newPrescription).subscribe(
         res => {
           // get defualt value before reset
-          const userId = this.userId.value;
+          const user = this.user.value;
           const date = this.today;
           const professionalFullname = this.professionalFullname.value;
           professionalNgForm.resetForm();
           professionalForm.reset({
-            user_id: userId,
+            user: user,
             date: date,
             professionalFullname: professionalFullname
           });
@@ -173,8 +173,8 @@ export class ProfessionalFormComponent implements OnInit {
     }
   }
 
-  get userId(): AbstractControl{
-    return this.professionalForm.get('user_id');
+  get user(): AbstractControl{
+    return this.professionalForm.get('user');
   }
 
   get date(): AbstractControl{
