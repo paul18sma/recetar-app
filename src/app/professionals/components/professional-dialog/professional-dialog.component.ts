b2b2a67ff6e5af8bbbfd8f508f4c1eca17c34be5
eugesma@gmail.com <eugesma@gmail.com>
2020-05-10 01:38:34 -0300

    style: add dispensed error dialog

--- src/app/professionals/components/professional-dialog/professional-dialog.component.ts
+++ src/app/professionals/components/professional-dialog/professional-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject, } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
 import { Prescriptions } from '@interfaces/prescriptions';
-import { ProfessionalListComponent } from '../professional-list/professional-list.component';
+import { InteractionService } from "@professionals/interaction.service";
 
 @Component({
   selector: 'app-professional-dialog',
@@ -11,14 +11,14 @@ import { ProfessionalListComponent } from '../professional-list/professional-lis
   animations: [
     fadeInOnEnterAnimation(), 
     fadeOutOnLeaveAnimation()
-  ]
+  ],
 })
 export class ProfessionalDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<ProfessionalDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    private professionalList: ProfessionalListComponent
+    private _interactionService: InteractionService
   ) {}
 
   ngOnInit(): void {
@@ -29,7 +29,8 @@ export class ProfessionalDialogComponent implements OnInit {
   }
 
   deletePrescription(prescription: Prescriptions){
-    this.professionalList.deletePrescription(prescription);
+    this._interactionService.deletePrescription(prescription);
+    this.dialogRef.close();
   }
 }
 
