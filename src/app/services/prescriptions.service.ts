fedc7ba57ad5db10f965442c0adb748737c06871
paul <paul.18.sma@gmail.com>
2020-05-16 20:57:28 -0300

    improves create / update / delete requests

--- src/app/services/prescriptions.service.ts
+++ src/app/services/prescriptions.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Prescriptions } from "../interfaces/prescriptions";
 import { AuthService } from '@auth/services/auth.service';
+import { tap, mapTo } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrescriptionsService {
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  private myPrescriptions: BehaviorSubject<Prescriptions[]>;
+  private prescriptionsArray: Prescriptions[] = [];
 
-  getPrescriptions(): Observable<any>{
-    return this.http.get(`${environment.API_END_POINT}/prescriptions`);
-  }
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.myPrescriptions = new BehaviorSubject<Prescriptions[]>(this.prescriptionsArray);
+   }
 
-  getById(id: string): Observable<Prescriptions>{
-    return this.http.get<Prescriptions>(`${environment.API_END_POINT}/prescriptions/${id}`);
+
+  // getPrescriptions(): Observable<any>{
+  //   return this.http.get(`${environment.API_END_POINT}/prescriptions`);
+  // }
+
+  getById(id: string): Observable<Prescriptions[]>{
+    return this.http.get<Prescriptions[]>(`${environment.API_END_POINT}/prescriptions/${id}`);
   }
 
   dispense(prescriptionId: string): Observable<Prescriptions> {
@@ -29,19 +36,60 @@ export class PrescriptionsService {
     return this.http.get<Prescriptions[]>(`${environment.API_END_POINT}/prescriptions/find/${params.patient_dni}&${params.dateFilter}`);
   }
 
-  getByUserId(userId: string): Observable<Prescriptions[]> {
-    return this.http.get<Prescriptions[]>(`${environment.API_END_POINT}/prescriptions/get-by-user-id/${userId}`);
+  getByUserId(userId: string): Observable<Boolean> {
+    return this.http.get<Prescriptions[]>(`${environment.API_END_POINT}/prescriptions/get-by-user-id/${userId}`).pipe(
+      tap((prescriptions: Prescriptions[]) => this.setPrescriptions(prescriptions)),
+      mapTo(true)
+    );
   }
 
-  newPrescription(prescription: Prescriptions): Observable<Prescriptions> {
-    return this.http.post<Prescriptions>(`${environment.API_END_POINT}/prescriptions`, prescription);
+  newPrescription(prescription: Prescriptions): Observable<Boolean> {
+    return this.http.post<Prescriptions>(`${environment.API_END_POINT}/prescriptions`, prescription).pipe(
+      tap((newPrescription: Prescriptions) => this.addPrescription(newPrescription)),
+      mapTo(true)
+    );
   }
 
-  editPrescription(prescription: Prescriptions): Observable<Prescriptions> {
-    return this.http.patch<Prescriptions>(`${environment.API_END_POINT}/prescriptions/${prescription._id}`, prescription);
+  editPrescription(prescription: Prescriptions): Observable<Boolean> {
+    return this.http.patch<Prescriptions>(`${environment.API_END_POINT}/prescriptions/${prescription._id}`, prescription).pipe(
+      tap((updatedPrescription: Prescriptions) => this.updatePrescription(updatedPrescription)),
+      mapTo(true)
+    );
   }
 
-  deletePrescription(prescriptionId: string): Observable<Prescriptions> {
-    return this.http.delete<Prescriptions>(`${environment.API_END_POINT}/prescriptions/${prescriptionId}`)
+  deletePrescription(prescriptionId: string): Observable<Boolean> {
+    return this.http.delete<Prescriptions>(`${environment.API_END_POINT}/prescriptions/${prescriptionId}`).pipe(
+      tap(() => this.removePrescription(prescriptionId)),
+      mapTo(true)
+    );
+  }
+
+  private setPrescriptions(prescriptions: Prescriptions[]){
+    this.prescriptionsArray.push(...prescriptions);
+    this.myPrescriptions.next(prescriptions);
+  }
+
+
+  private addPrescription(prescription: Prescriptions){
+    this.prescriptionsArray.push(prescription);
+    this.myPrescriptions.next(this.prescriptionsArray);
+  }
+
+  private removePrescription(removedPrescription: string){
+    const removeIndex = this.prescriptionsArray.findIndex((prescription: Prescriptions) => prescription._id === removedPrescription);
+
+    this.prescriptionsArray.splice(removeIndex, 1);
+    this.myPrescriptions.next(this.prescriptionsArray);
+  }
+
+  private updatePrescription(updatedPrescription: Prescriptions){
+    const updateIndex = this.prescriptionsArray.findIndex((prescription: Prescriptions) => prescription._id === updatedPrescription._id);
+
+    this.prescriptionsArray.splice(updateIndex, 1, updatedPrescription);
+    this.myPrescriptions.next(this.prescriptionsArray);
+  }
+
+  get prescriptions(): Observable<Prescriptions[]> {
+    return this.myPrescriptions.asObservable();
   }
 }
