6b22a31109e514b4da9641076fa24377d8f9e18f
eugesma@gmail.com <eugesma@gmail.com>
2020-05-20 00:01:53 -0300

    feat: add adapter to MPI

--- src/app/interfaces/patients.ts
+++ src/app/interfaces/patients.ts
@@ -1,7 +1,33 @@
-export interface Patient {
-  _id?: string;
-  dni?: string;
-  firstName: string;
-  lastName: string;
-  sex: string;
+import { Injectable } from "@angular/core";
+import { Adapter } from "./adapter";
+
+export class Patient {
+  constructor(
+    public lastName: string,
+    public firstName: string,
+    public sex: string,
+    public status: string,
+    public dni?: string,
+    public createdAt?: Date
+    ) {}
+    public _id?: string;
+}
+
+
+
+
+@Injectable({
+  providedIn: "root"
+})
+export class PatientAdapter implements Adapter<Patient> {
+  adapt(item: any): Patient {
+    return new Patient(
+      item.apellido,
+      item.nombre,
+      item.estado,
+      item.sexo[0].toUpperCase() + item.sexo.substr(1).toLowerCase(),
+      item.documento,
+      new Date()
+    );
+  }
 }
