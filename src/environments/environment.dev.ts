55cd36de31adea9cf3852aba98cef00dee8af9a1
eugesma@gmail.com <eugesma@gmail.com>
2020-05-20 00:03:01 -0300

    feat: add jwt token and Andes MPI api

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   API_END_POINT: 'https://prescription-validator-api.herokuapp.com/api',
-  ANDES_API: 'https://app.andes.gob.ar/api'
+  ANDES_API: 'https://app.andes.gob.ar/api',
+  ANDES_TEST_API: 'https://test.andes.gob.ar/api',
 };
